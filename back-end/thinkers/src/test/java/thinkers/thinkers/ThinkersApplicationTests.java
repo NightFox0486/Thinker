c4c98eacf2398824d84ab605732c8821bd86b226
Jin.B <mrjin486@gmail.com>
2024-01-22 17:03:33 +0900

    ✨ [feat] Setup Back-End

---
+++ back-end/thinkers/src/test/java/thinkers/thinkers/ThinkersApplicationTests.java
@@ -0,0 +1,13 @@
+package thinkers.thinkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
