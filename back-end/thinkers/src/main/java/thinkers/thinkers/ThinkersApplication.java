c1fac2fbe5b8ee3beb8fb3e9fd7b997622fac70a
Jin.B <mrjin486@gmail.com>
2024-02-05 21:20:47 +0900

    ✨ temp backup

--- back-end/thinkers/src/main/java/thinkers/thinkers/ThinkersApplication.java
+++ back-end/thinkers/src/main/java/thinkers/thinkers/ThinkersApplication.java
@@ -2,8 +2,10 @@ package thinkers.thinkers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/properties/env.properties")
 public class ThinkersApplication {
 
 	public static void main(String[] args) {
